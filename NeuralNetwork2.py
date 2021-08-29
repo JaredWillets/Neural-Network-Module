feb568e67b9b732921af0001654d604af7891f32
Jared Willets <61078848+JaredWillets@users.noreply.github.com>
2021-08-29 19:48:55 +0900

    Added forward propagation/ prediction capabilities

--- NeuralNetwork2.py
+++ NeuralNetwork2.py
@@ -1,11 +1,13 @@
 import json
 from random import random as rand
+from math import euler
 class NeuralNetwork:
     def __init__(self,structure):
         self.sumError = float()
         self.network = []
         self.trained = False
         self.iterationNumber = 0
+        self.structure = structure
         for layerNumber, layer in enumerate(structure[1:]):
             newLayer = []
             for neuronNumber in range(layer):
@@ -21,3 +23,20 @@ class NeuralNetwork:
         with open(f'{directory}.neural','w') as file:
             json.dump(self.network, file)
             file.close()
+    def predict(self, data, **kwargs):
+        if 'print_out' in kwargs:
+            print_out = True
+        inputs = data
+        newInputs = []
+        for layerNumber, layer in enumerate(self.network[1:]):
+            for neuronNumber, neuron in enumerate(layer):
+                for weightNumber, weight in enumerate(neuron['weights']):
+                    neuron['output'] += weight*inputs[weightNumber]
+                    neuron['output'] = 1/(1+(math.e**(-float(neuron['output']))))
+                    newInputs.append(neuron['output'])
+            inputs = newInputs
+        output = inputs
+        if print_out:
+            print(output)
+        return output
+                    
